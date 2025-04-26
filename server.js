2ce453952a25ce5310fa11041705b068fb5bd9ae
Ayush Shiroya <shiroyaayush123@gmail.com>
2025-04-26 18:41:07 +0530

    PORT change

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+app.use(cors());
+app.use(express.json());
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
